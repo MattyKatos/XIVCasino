743e993fad4ff9a5f8521e42f1926395671502da
Matty Katos <mattwonus@gmail.com>
2023-11-06 17:37:06 -0500

    updated highlow

--- commands/highlow.js
+++ commands/highlow.js
@@ -1,11 +1,6 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 const fs = require('node:fs');
 
-//Adjust Money
-function adjustBalance(){
-	
-}
-
 module.exports = {
 
 	data: new SlashCommandBuilder()
@@ -22,6 +17,19 @@ module.exports = {
 		const bet = Number(interaction.options.getString('bet'))
 
 		//Define Buttons and rows
+		const start = new ButtonBuilder()
+			.setCustomId('high')
+			.setLabel('High')
+			.setStyle(ButtonStyle.Secondary);
+
+		const cancel = new ButtonBuilder()
+			.setCustomId('low')
+			.setLabel('Low')
+			.setStyle(ButtonStyle.Secondary);
+
+		const startrow = new ActionRowBuilder()
+			.addComponents(high, low);
+
 		const high = new ButtonBuilder()
 			.setCustomId('high')
 			.setLabel('High')
@@ -32,7 +40,7 @@ module.exports = {
 			.setLabel('Low')
 			.setStyle(ButtonStyle.Secondary);
 
-		const row = new ActionRowBuilder()
+		const betrow = new ActionRowBuilder()
 			.addComponents(high, low);
 
 		//Check to see if user exists
@@ -52,13 +60,17 @@ module.exports = {
 			interaction.reply({ content: 'You can\'t bet more gil then you have in your wallet!', ephemeral: true });
 			console.log('[' + DiscordUsername + '#' + DiscordID + '] HIGHLOW - 401: User can\'t cover bet.')
 			return
-		}
+		} else {}
+
+		//Confirm Bet
+
 
 		//Gamble
 		num1 = Math.floor(Math.random() * 11)
 		num2 = Math.floor(Math.random() * 11)
-		newBalance = Number(userWallet.Balance) - bet
-		
+		newBalance = Number() - bet
+
+
 		interaction.reply({ content: 'Your current balance is ' + userWallet.Balance + ' gil.', ephemeral: true });
 		console.log('[' + DiscordUsername + '#' + DiscordID + '] HIGHLOW - 200: User can\'t afford the bet.')
 	},
