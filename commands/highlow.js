930cf08c1bad0ef2ae6ecb989853b0f2378f92d8
Matty Katos <mattwonus@gmail.com>
2023-11-06 17:23:41 -0500

    highlow

--- commands/highlow.js
+++ commands/highlow.js
@@ -1,5 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 const fs = require('node:fs');
+
+//Adjust Money
+function adjustBalance(){
+	
+}
 
 module.exports = {
 
@@ -8,13 +13,27 @@ module.exports = {
 		.setDescription('Bet on whether the second card to be drawn from the deck will be higher or lower than the first.')
 		.addStringOption(option =>
 			option.setName('bet')
-			.setDescription('The amount of gil you\'d like to bet.')
-			.setRequired(true)),
+				.setDescription('The amount of gil you\'d like to bet.')
+				.setRequired(true)),
 
 	async execute(interaction) {
 		const DiscordID = interaction.user.id
 		const DiscordUsername = interaction.user.username
 		const bet = Number(interaction.options.getString('bet'))
+
+		//Define Buttons and rows
+		const high = new ButtonBuilder()
+			.setCustomId('high')
+			.setLabel('High')
+			.setStyle(ButtonStyle.Secondary);
+
+		const low = new ButtonBuilder()
+			.setCustomId('low')
+			.setLabel('Low')
+			.setStyle(ButtonStyle.Secondary);
+
+		const row = new ActionRowBuilder()
+			.addComponents(high, low);
 
 		//Check to see if user exists
 		try {
@@ -29,17 +48,18 @@ module.exports = {
 		//Can user cover bet?
 		userWallet = JSON.parse(fs.readFileSync('./cache/users/' + DiscordID + '.json'))
 		userBalance = Number(userWallet.Balance)
-		if (bet > userBalance){
+		if (bet > userBalance) {
 			interaction.reply({ content: 'You can\'t bet more gil then you have in your wallet!', ephemeral: true });
+			console.log('[' + DiscordUsername + '#' + DiscordID + '] HIGHLOW - 401: User can\'t cover bet.')
 			return
-		}else{
-			newBalance = Number(userWallet.Balance) - bet
-			newUserWallet = '{"DiscordID":"' + userWallet.DiscordID + '","DiscordUsername":"' + userWallet.DiscordUsername + '","Balance":'+newBalance+'}'
-			fs.writeFileSync('./cache/users/' + DiscordID + '.json', newUserWallet)
 		}
 
 		//Gamble
+		num1 = Math.floor(Math.random() * 11)
+		num2 = Math.floor(Math.random() * 11)
+		newBalance = Number(userWallet.Balance) - bet
+		
 		interaction.reply({ content: 'Your current balance is ' + userWallet.Balance + ' gil.', ephemeral: true });
-		console.log('[' + DiscordUsername + '#' + DiscordID + '] HIGHLOW - 401: User can\'t afford the bet.')
+		console.log('[' + DiscordUsername + '#' + DiscordID + '] HIGHLOW - 200: User can\'t afford the bet.')
 	},
 };
